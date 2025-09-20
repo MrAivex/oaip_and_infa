6453b5682e0e0d8424d44894be66093a575d18d5
GrafAleksey315 <lekstar_67@mail.ru>
2025-09-20 23:12:49 +0300

    Дз на 3й семинар часть 1

--- domashnie_raboty/oaip/homework_240925/home_number_6.cpp
+++ domashnie_raboty/oaip/homework_240925/home_number_6.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-    float arr[5];
+    float arr[15];
     float min = 0;
     int imin = 0;
     float max = 0;
     int imax = 0;
     int flag = 0;
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 15; i++)
     {
         cout << "Введите число: ";
         cin >> arr[i];
@@ -35,7 +35,7 @@ int main()
 
     if (imin == 1 && imax == 0)
     {
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < 15; j++)
         {
             arr[j] = arr[j] / 2;
             cout << arr[j] << " ";
--- domashnie_raboty/oaip/homework_240925/home_number_8.cpp
+++ domashnie_raboty/oaip/homework_240925/home_number_8.cpp
@@ -3,21 +3,22 @@ using namespace std;
 
 int main()
 {
-    int arr[5];
+    int arr[7];
     int min = 0;
-    int max = 0;
     int imin = 0;
+    int max = 0;
+    int imax = 0;
     int flag = 0;
 
-    for (int i = 0; i < 15; i++)
+    for (int i = 0; i < 7; i++)
     {
         cout << "Введите число: ";
         cin >> arr[i];
 
         if (flag == 0)
         {
-            max = arr[i];
             min = arr[i];
+            max = arr[i];
             flag++;
         }
 
@@ -30,25 +31,26 @@ int main()
         else if (arr[i] > max)
         {
             max = arr[i];
+            imax = i;
         }
     }
 
-    for (int i = 0; i < 15; i++)
+    for (int i = 0; i < 7; i++)
     {
         cout << arr[i] << " ";
     }
 
     cout << endl;
 
-    if (min == 0 || max == 0)
+    if (imin == 4 && imax == 1)
     {
-        for (int i = 0; i < imin; i++)
+        for (int i = imin + 1; i < 7; i++)
         {
-            arr[i] = 0;
+            arr[i] = max;
         }
     }
 
-    for (int i = 0; i < 15; i++)
+    for (int i = 0; i < 7; i++)
     {
         cout << arr[i] << " ";
     }
