8ddaed27c114a369b6bdf5054de078ca02296af6
GrafAleksey315 <lekstar_67@mail.ru>
2025-10-05 23:18:34 +0300

    add part of dz for seminar 5

---
+++ domashnie_raboty/oaip/seminar_5_dz/nomer_1__sem5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> myVector;
+
+    for (int i = 0; i < 10; i++)
+    {
+        myVector.push_back(i);
+    }
+
+    auto coutVector = [&myVector]()
+    {
+        for (int elem : myVector)
+        {
+            cout << elem << " ";
+        }
+    };
+
+    coutVector();
+
+    return 0;
+}
