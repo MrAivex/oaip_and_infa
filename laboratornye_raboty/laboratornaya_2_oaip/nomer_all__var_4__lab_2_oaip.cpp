ad3f25333c7f7eefe053784ce7c54bf37d6c9f3b
GrafAleksey315 <lekstar_67@mail.ru>
2025-10-25 11:48:01 +0300

    laba2

--- laboratornye_raboty/laboratornaya_2_oaip/nomer_all__var_4__lab_2_oaip.cpp
+++ laboratornye_raboty/laboratornaya_2_oaip/nomer_all__var_4__lab_2_oaip.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -135,31 +137,80 @@ int count_coincidences(char *ptr)
 }
 
 // ___________________________________| № 6
-void find_longest_substr(char *ptr)
+string find_longest_substr(string str)
 {
-    char substr[] = "";
-    while (*ptr != '\0')
+    string cur_str = "";
+    vector<string> arr;
+    for (char s : str)
     {
-        char str1[100] = "";
-        if (*ptr == ' ')
+        if (s != ' ')
         {
-
+            cur_str = cur_str + s;
         }
         else
         {
-            strncpy(str1, ptr, 1);
+            arr.insert(arr.end(), cur_str);
+            cur_str = "";
+        }
+    }
+
+    arr.insert(arr.end(), cur_str);
+
+    string max_substr = "";
+
+    // for (string word1 : arr)
+    // {
+        // for (string word2 : arr)
+        // {
+        //     if (word1 != word2)
+        //     {
+        //         for (int i = 0; i < word1.length(); ++i)
+        //         {
+        //             for (int j = i; j < word1.length(); ++j)
+        //             {
+        //                 string word_part = word1.substr(i, j);
+        //                 if (word2.find(word_part) != string::npos && word_part.length() > max_substr.length())
+        //                 {
+        //                     max_substr = word_part;
+        //                 }
+        //             }  
+        //         }
+        //     }
+        //     else
+        //     {
+                
+        //     }
+        // }
+
+        for (int elem = 0; elem < arr.size() - 1; ++elem)
+        {
+            string word1 = arr.at(elem);
+            string word2 = arr.at(elem + 1);
+            for (int i = 0; i < word1.length(); ++i)
+                {
+                    for (int j = i; j < word1.length(); ++j)
+                    {
+                        string word_part = word1.substr(i, j + 1);
+                        if (word2.find(word_part) != string::npos && word_part.length() > max_substr.length())
+                        {
+                            max_substr = word_part;
+                        }
+                    }  
+                }
         }
 
-        cout << str1 << endl;
-
-        ++ptr;
-    }
+    return max_substr;
 }
 
 int main()
 {
     char myString[] = "stem cob flower leaf shrub bulb root herb branch tree";
     char *ptrMyString = myString;
+    string myStr;
+    vector<string> strArr;
+
+    cout << "Введите 10 слов: ";
+    getline(cin, myStr);
 
     // № 1
     cout << "Количество слов, оканчивающихся на b, равно: " << count_b_in_end(ptrMyString) << endl;
@@ -177,7 +228,7 @@ int main()
     cout << "Количество слов, у которых совпадает второй и предпоследний символ, равно: " << count_coincidences(ptrMyString) << endl;
 
     // № 6
-    find_longest_substr(ptrMyString);
+    cout << "Самая длинная общая подстрока: " << find_longest_substr(myStr) << endl;
 
     return 0;
 }
